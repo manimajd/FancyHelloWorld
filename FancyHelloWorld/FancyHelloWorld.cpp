5d2bd6e17512d9f5733d93fb959e1fa2a44b8674
Mani Majd <mani.majd@gmail.com>
2018-06-16 19:31:20 -0700

    Add files via upload

---
+++ FancyHelloWorld/FancyHelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "//////////////////////////////" << endl;
+	cout << "|| Hello World By Mani Majd ||" << endl;
+	cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << endl;
+}
---
+++ FancyHelloWorld/FancyHelloWorld.java
@@ -0,0 +1,11 @@
+
+public class FancyHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("//////////////////////////////");
+		System.out.println("|| Hello World By Mani Majd ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+	}
+
+}
